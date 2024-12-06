a312a7aea2075b1e7942bef15becfeb590003915
ralphkian <ralph.kjian@hotmail.com>
2024-12-06 17:41:15 +0200

    Initializing the project

---
+++ Auth.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+
+namespace YandexTaxi
+{
+    public class Auth
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int age { get; set; }
+        public Gender? gender { get; set; }
+
+        // Constructor for Auth class with age check
+        public Auth(string n, string e, string p, int a)
+        {
+            this.name = n;
+            this.email = e;
+            this.password = p;
+            this.age = a;
+
+            if (a < 18)
+            {
+                int future = 18 - a;  // Calculated when can the user have access to our app ( just for fun )
+                MessageBox.Show($"Sorry {n}, Minors aren't allowed to use Yandex app\nMaybe see you {future} year/s later !");
+                Environment.Exit(0);  // The user can't continue accessing our app if he's a minor
+            }
+
+            MessageBox.Show($"Welcome {n} to our App\n", "Success");
+        }
+
+        // Overloaded constructor with gender
+        public Auth(string n, string e, string p, int a, Gender g)
+            : this(n, e, p, a)
+        {
+            this.gender = g;
+        }
+
+        // Overriding ToString method to display user details
+        public override string ToString()
+        {
+            return $"Name: {name}\n" +
+                $"Email: {email}\n" +
+                $"Password: {password}\n" +
+                $"Age: {age}\n" +
+                $"Gender: {gender}\n";
+        }
+    }
+
+    // Enum for Gender
+    public enum Gender
+    {
+        male,
+        female
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using YandexTaxi;
+
+class Program
+{
+    static void Main()
+    {
+        // Authentication attributes
+        string name;
+        string email;
+        string password;
+        int age;
+        Gender gender;
+
+        Console.WriteLine("Bus Taxi App\n");
+        Console.WriteLine("\nAuthentication\n");
+
+        Console.WriteLine("Enter your Name: ");
+        name = Console.ReadLine();
+
+        Console.WriteLine("\nEnter your Email: ");
+        email = Console.ReadLine();
+
+        Console.WriteLine("\nEnter your Password: ");
+        password = Console.ReadLine();
+
+        Console.WriteLine("\nEnter your Age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nEnter your Gender: ");
+        Console.WriteLine("0. Male");
+        Console.WriteLine("1. Female");
+        int genderInput = Convert.ToInt32(Console.ReadLine());
+        gender = (Gender)genderInput;
+
+        Auth User = new Auth(name, email, password, age, gender);
+        MessageBox.Show(User.ToString(), "User Info");
+    }
+
+}
